3e3c90cee913a3e3039d1c80d3453a8a613fd630
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-16 17:38:11 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """In a text file, there is a single character H.
 The text editor can execute only two operations in this file.Copy All and Paste
 Given a number n, write a method that calculates
@@ -8,17 +8,18 @@ n * H characters in the file.
 
 
 def minOperations(n: int) -> int:
-    """Returns the minimum number of operations needed to achieve n H characters in the text file"""
+    """Returns the minimum number of operations needed to
+    achieve n H characters in the text file"""
     if n <= 1:
         return 0
-    
+
     value = 0
     prime_num = 2
-    
+
     while n > 1:
         while n % prime_num == 0:
             value += prime_num
             n //= prime_num
         prime_num += 1
-    
+
     return value
