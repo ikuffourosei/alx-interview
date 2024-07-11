61ff0efa43c2e2bea5f13ae90c9324c0b1d7d210
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-11 08:44:41 +0000

    NQUEENS

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def print_usage_and_exit():
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+
+def is_integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def solve_n_queens(n):
+    def is_safe(board, row, col):
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
+
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        return True
+
+    def solve_recursively(board, col):
+        if col >= n:
+            solutions.append([[i, row.index(1)] for i, row in enumerate(board)])
+            return
+
+        for i in range(n):
+            if is_safe(board, i, col):
+                board[i][col] = 1
+                solve_recursively(board, col + 1)
+                board[i][col] = 0
+
+    solutions = []
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solve_recursively(board, 0)
+    return solutions
+
+
+def main():
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
+
+    arg = sys.argv[1]
+    if not is_integer(arg):
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(arg)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_n_queens(N)
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    main()
