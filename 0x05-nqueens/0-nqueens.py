aafaf7454f37ee721785591bc1d803a61e53ff0b
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-11 09:03:27 +0000

    NQUEENS

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,53 @@
 #!/usr/bin/env python3
-"""Nqueens"""
+"""N QUEENS"""
 import sys
 
 
-def print_usage_and_exit():
-    """Shows the use and exit with code 1"""
-    print("Usage: nqueens N")
-    sys.exit(1)
+def nqueens():
+    """A function that defines the moves of a queen in a chess"""
+    def print_usage_and_exit():
+        print("Usage: nqueens N")
+        sys.exit(1)
 
+    def is_integer(s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
 
-def is_integer(s) -> bool:
-    """Checks if a given parameter is an integer"""
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+    def solve_n_queens(n):
+        def is_safe(board, row, col):
+            for i in range(col):
+                if board[row][i] == 1:
+                    return False
 
+            for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+                if board[i][j] == 1:
+                    return False
 
-def solve_n_queens(n):
-    """Solves the problem"""
-    def is_safe(board, row, col):
-        """CHecks if a slot on the board is safe for the Queen"""
-        for i in range(col):
-            if board[row][i] == 1:
-                return False
+            for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+                if board[i][j] == 1:
+                    return False
 
-        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-            if board[i][j] == 1:
-                return False
+            return True
 
-        for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-            if board[i][j] == 1:
-                return False
+        def solve_recursively(board, col):
+            if col >= n:
+                val.append([[i, row.index(1)] for i, row in enumerate(board)])
+                return
 
-        return True
+            for i in range(n):
+                if is_safe(board, i, col):
+                    board[i][col] = 1
+                    solve_recursively(board, col + 1)
+                    board[i][col] = 0
 
-    def solve_recursively(board, col):
-        if col >= n:
-            res.append([[i, row.index(1)] for i, row in enumerate(board)])
-            return
+        val = []
+        board = [[0 for _ in range(n)] for _ in range(n)]
+        solve_recursively(board, 0)
+        return val
 
-        for i in range(n):
-            if is_safe(board, i, col):
-                board[i][col] = 1
-                solve_recursively(board, col + 1)
-                board[i][col] = 0
-
-    res = []
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solve_recursively(board, 0)
-    return res
-
-
-def main():
     if len(sys.argv) != 2:
         print_usage_and_exit()
 
@@ -73,4 +67,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    nqueens()
