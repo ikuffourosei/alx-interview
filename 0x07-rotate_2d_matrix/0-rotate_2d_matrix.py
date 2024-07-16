19aed694356aeb15186d95bf05d2d8cccf96a21a
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-16 20:31:24 +0000

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,14 +4,12 @@
 
 def rotate_2d_matrix(matrix):
     """A function that rotates a matrix by 90degrees"""
-    # n x n matrix
-    row = len(matrix)
-
-    # find transpose
-    for i in range(row):
-        for j in range(i, row):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # reverse matrix
-    for i in range(row):
-        matrix[i].reverse()
+    if isinstance(matrix, list):
+        row = len(matrix)
+        for i in range(row):
+            for j in range(i, row):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(row):
+            matrix[i].reverse()
+    else:
+        return
