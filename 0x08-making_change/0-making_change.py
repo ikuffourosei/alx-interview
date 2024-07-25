00a7818dc3ea096f07ac5736829cfb1f56ea4440
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-25 19:45:47 +0000

    fixed pycodestyle errors

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Making Change"""
 
+
 def makeChange(coins, total):
     """Returns the fewest number of coins to meet total"""
     if total <= 0:
         return 0
-    
+
     # Sort coins in descending order
     coins.sort(reverse=True)
-    
+
     count = 0
     for coin in coins:
         if total == 0:
@@ -17,5 +18,5 @@ def makeChange(coins, total):
             num_coins = total // coin
             count += num_coins
             total -= num_coins * coin
-    
+
     return count if total == 0 else -1
